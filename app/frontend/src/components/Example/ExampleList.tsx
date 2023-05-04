4aa312d096c1f13314c8e298895a4fc03a534fb5
Darcy Thomas <dthomas@raygun.com>
2023-05-04 16:37:31 +1200

    moar examples

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "How do I add a user with raygun RUM?",
+        value: "How do I add a user with raygun RUM?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What can you tell me about crash reporting?", value: "What can you tell me about crash reporting?" },
+    { text: "How does alerting work?", value: "How does alerting work?" }
 ];
 
 interface Props {
